df95bfb3b718292c882472a79d2765ae8b2fc4c6
simone-alcala <simone.alcala@gmail.com>
2022-05-29 19:01:10 -0300

    Prepare for deploy

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,18 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 dotenv.config();
-
+/*
 const db = new Pool ({ connectionString: process.env.DATABASE_URL });
+
+export default db;*/
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
